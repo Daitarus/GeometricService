d2dc951fee20992c20c24ef1886418fa30d4ec3c
PavelChaikovskii <chajkovskij_99@mail.ru>
2024-08-04 20:27:22 +0300

    refactor: rename services to handlers

---
+++ GeometricService.Domain/Handlers/Interfaces/ICalculationAreaHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace GeometricService.Domain.Handlers.Interfaces
+{
+    public interface ICalculationAreaHandler<T> where T : class
+    {
+        public double CalculateArea(T figure);
+    }
+}
--- GeometricService.Domain/Services/Interfaces/IGeometricCalculationService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GeometricService.Domain.Services.Interfaces
-{
-    public interface IGeometricCalculationService<T> where T : class
-    {
-        public double CalculateArea(T figure);
-    }
-}
