288b71df99d9128a19ed8efbd8983618e570e5b1
PavelChaikovskii <chajkovskij_99@mail.ru>
2024-08-04 20:27:53 +0300

    feat: add new CalculationAreaHandlers for figures

---
+++ GeometricService.Domain/Handlers/RectangleCalculationService.cs
@@ -0,0 +1,15 @@
+﻿using GeometricService.Domain.Models;
+using GeometricService.Domain.Handlers.Interfaces;
+
+namespace GeometricService.Domain.Handlers
+{
+    public class RectangleCalculationHandler : ICalculationAreaHandler<Rectangle>
+    {
+        public double CalculateArea(Rectangle figure)
+        {
+            if (figure == null) throw new ArgumentNullException(nameof(figure));
+
+            return figure.Height * figure.Width;
+        }
+    }
+}
